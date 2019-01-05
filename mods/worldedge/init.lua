1d062863be946f851a597eb058b65587e86bdd22
Coder12a <38924418+Coder12a@users.noreply.github.com>
2019-01-05 11:30:28 -0600

    Limit the map size to 3k

--- mods/worldedge/init.lua
+++ mods/worldedge/init.lua
@@ -2,7 +2,7 @@
 -- TODO: Check for terrain height
 
 -- Defines the edge of a world
-local edge = tonumber(minetest.settings:get("world_edge")) or 30000
+local edge = 3000
 -- Radius which should be checked for a good teleportation place
 local radius = 2
 --------------
