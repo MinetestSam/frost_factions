928a9c263dc9ff67b6afc2312b0b2ea89375a746
Coder12a <38924418+Coder12a@users.noreply.github.com>
2019-01-07 17:22:32 -0600

    Make bone's share time 30 seconds
    And make the formspec bigger.

--- mods/bones/init.lua
+++ mods/bones/init.lua
@@ -10,18 +10,18 @@ local function is_owner(pos, name)
 end
 
 local bones_formspec =
-	"size[8,9]" ..
+	"size[8,11]" ..
 	default.gui_bg ..
 	default.gui_bg_img ..
 	default.gui_slots ..
-	"list[current_name;main;0,0.3;8,4;]" ..
-	"list[current_player;main;0,4.85;8,1;]" ..
-	"list[current_player;main;0,6.08;8,3;8]" ..
+	"list[current_name;main;0,0.3;8,6;]" ..
+	"list[current_player;main;0,6.85;8,1;]" ..
+	"list[current_player;main;0,8.08;8,3;8]" ..
 	"listring[current_name;main]" ..
 	"listring[current_player;main]" ..
-	default.get_hotbar_bg(0,4.85)
+	default.get_hotbar_bg(0,6.85)
 
-local share_bones_time = tonumber(minetest.settings:get("share_bones_time")) or 1200
+local share_bones_time = tonumber(minetest.settings:get("share_bones_time")) or 30
 local share_bones_time_early = tonumber(minetest.settings:get("share_bones_time_early")) or share_bones_time / 4
 
 minetest.register_node("bones:bones", {
