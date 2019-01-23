7c4a6fc7a747afc9c10402735c6591af868e0bee
Coder12a <38924418+Coder12a@users.noreply.github.com>
2019-01-23 14:56:28 -0600

    Update item entity
    Sometimes the item will not start the hopper

--- mods/default/item_entity.lua
+++ mods/default/item_entity.lua
@@ -2,10 +2,12 @@
 
 local builtin_item = minetest.registered_entities["__builtin:item"]
 
+local time_to_live = tonumber(core.settings:get("item_entity_ttl"))
+if not time_to_live then
+	time_to_live = 900
+end
+
 local item = {
-	old_pos = nil,
-	checked = false,
-	
 	set_item = function(self, itemstring)
 		builtin_item.set_item(self, itemstring)
 
@@ -43,9 +45,55 @@ local item = {
 	end,
 
 	on_step = function(self, dtime)
-		builtin_item.on_step(self, dtime)
-		
 		local object = self.object
+		
+		self.age = self.age + dtime
+		if time_to_live > 0 and self.age > time_to_live then
+			self.itemstring = ''
+			object:remove()
+			return
+		end
+		local p = object:getpos()
+		p.y = p.y - 0.5
+		local node = core.get_node_or_nil(p)
+		local name = node.name
+		-- Delete in 'ignore' nodes
+		if node and name == "ignore" then
+			self.itemstring = ""
+			object:remove()
+			return
+		end
+
+		-- If node is nil (unloaded area), or node is not registered, or node is
+		-- walkably solid and item is resting on nodebox
+		local v = object:getvelocity()
+		if not node or not core.registered_nodes[name] or
+				core.registered_nodes[name].walkable and v.y == 0 then
+			if self.physical_state then
+				local own_stack = ItemStack(object:get_luaentity().itemstring)
+				-- Merge with close entities of the same item
+				for _, object in ipairs(core.get_objects_inside_radius(p, 0.8)) do
+					local obj = object:get_luaentity()
+					if obj and obj.name == "__builtin:item"
+							and obj.physical_state == false then
+						if self:try_merge_with(own_stack, object, obj) then
+							return
+						end
+					end
+				end
+				object:setvelocity({x = 0, y = 0, z = 0})
+				object:setacceleration({x = 0, y = 0, z = 0})
+				self.physical_state = false
+				object:set_properties({physical = false})
+			end
+		else
+			if not self.physical_state then
+				object:setvelocity({x = 0, y = 0, z = 0})
+				object:setacceleration({x = 0, y = -10, z = 0})
+				self.physical_state = true
+				object:set_properties({physical = true})
+			end
+		end
 
 		if self.flammable then
 			-- flammable, check for igniters
@@ -53,18 +101,17 @@ local item = {
 			if self.ignite_timer > 10 then
 				self.ignite_timer = 0
 
-				local node = minetest.get_node_or_nil(object:getpos())
 				if not node then
 					return
 				end
 
 				-- Immediately burn up flammable items in lava
-				if minetest.get_item_group(node.name, "lava") > 0 then
+				if minetest.get_item_group(name, "lava") > 0 then
 					self:burn_up()
 				else
 					--  otherwise there'll be a chance based on its igniter value
 					local burn_chance = self.flammable
-						* minetest.get_item_group(node.name, "igniter")
+						* minetest.get_item_group(name, "igniter")
 					if burn_chance > 0 and math.random(0, burn_chance) ~= 0 then
 						self:burn_up()
 					end
@@ -72,29 +119,11 @@ local item = {
 			end
 		end
 	
-		local pos = object:getpos()
-		
-		if self.checked == true or not self.old_pos or self.old_pos.x ~= pos.x or self.old_pos.y ~= pos.y or self.old_pos.z ~= pos.z then
-			self.old_pos = pos
-			return
-		end
-		
-		self.checked = true
-		
-		local fpos = minetest.find_node_near(pos, 1.85, {"hopper:hopper", "hopper:hopper_side"}, true)
-		
-		if not fpos then
-			return
-		end
-		
-		local node = minetest.get_node_or_nil(fpos)
-		local inv = minetest.get_meta(fpos):get_inventory()
-		if not inv or not inv:room_for_item("main", ItemStack(object:get_luaentity().itemstring)) then
-			self.checked = false
-		end
-		local timer = minetest.get_node_timer(fpos)
-		if not timer:is_started() then
-			timer:start(1.0)
+		if name == "hopper:hopper" or name == "hopper:hopper_side" then
+			local timer = minetest.get_node_timer(p)
+			if not timer:is_started() then
+				timer:start(1.0)
+			end
 		end
 	end,
 }
